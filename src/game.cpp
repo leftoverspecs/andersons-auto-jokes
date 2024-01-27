b4714a8530f7dbf0489b1c158ba8defa3fd014aa
afx1337 <afx1337@afx1337>
2024-01-27 15:43:01 +0100

    Add audio to game.cpp

--- src/game.cpp
+++ src/game.cpp
@@ -2,17 +2,29 @@
 #include <SDL.h>
 
 #include <audio.h>
+#include <chunk.h>
+#include <music.h>
+
 #include <controller.h>
 #include <font.h>
 #include <image.h>
 
 #include <iostream>
+#include <cstdlib>
 
 #include "fight.h"
 #include "person.h"
 #include "shop.h"
 
 #include <family.png.h>
+#include <snd_laugh_child1.wav.h>
+#include <snd_laugh_female1.wav.h>
+#include <snd_laugh_female2.wav.h>
+#include <snd_laugh_male1.wav.h>
+#include <snd_laugh_male2.wav.h>
+#include <snd_laugh_male3.wav.h>
+#include <snd_steps.wav.h>
+#include <snd_steps2.wav.h>
 
 #include <boxyfont.h>
 #include <boxyfont.png.h>
@@ -29,6 +41,9 @@ const int WIDTH = 800;
 const int HEIGHT = 600;
 
 int main() {
+#ifndef WIN32
+    setenv("ALSA_CONFIG_DIR", "/usr/share/alsa", 1);
+#endif
     if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_GAMECONTROLLER | SDL_INIT_AUDIO) != 0) {
         std::cerr << SDL_GetError() << std::endl;
         return EXIT_FAILURE;
@@ -64,7 +79,14 @@ int main() {
     glClearColor(0.0, 0.0, 0.0, 0.0);
     glViewport(0, 0, WIDTH, HEIGHT);
 
-    //engine::Audio audio(44100, MIX_DEFAULT_FORMAT, 2, 128);
+    engine::Audio audio(44100, MIX_DEFAULT_FORMAT, 2, 64);
+    engine::Chunk snd_laugh_child1(snd_laugh_child1, sizeof(snd_laugh_child1));
+    engine::Chunk snd_laugh_female1(snd_laugh_female1, sizeof(snd_laugh_female1));
+    engine::Chunk snd_laugh_female2(snd_laugh_female2, sizeof(snd_laugh_female2));
+    engine::Chunk snd_laugh_male1(snd_laugh_male1, sizeof(snd_laugh_male1));
+    engine::Chunk snd_laugh_male2(snd_laugh_male2, sizeof(snd_laugh_male2));
+    engine::Chunk snd_laugh_male3(snd_laugh_male3, sizeof(snd_laugh_male3));
+    engine::Chunk snd_steps(snd_steps2, sizeof(snd_steps2));
 
     engine::Font font(WIDTH, HEIGHT, boxyfont, sizeof(boxyfont), assets::boxyfont_widths);
     engine::SpriteMap family_spritemap{family, sizeof(family), 8, 8};
