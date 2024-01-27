56b43ea0c1e70933ec13d489cda65de5a50586a3
Philipp Waehnert <philipp.waehnert@posteo.de>
2024-01-27 19:53:25 +0100

    Exports sound effects as vector

--- src/audio_data.cpp
+++ src/audio_data.cpp
@@ -1,4 +1,5 @@
 #include "audio_data.h"
+
 #include <snd_laugh_child1.wav.h>
 #include <snd_laugh_female1.wav.h>
 #include <snd_laugh_female2.wav.h>
@@ -9,11 +10,25 @@
 #include <snd_steps2.wav.h>
 
 namespace game {
-    const engine::Chunk chunk_snd_laugh_child1(snd_laugh_child1, sizeof(snd_laugh_child1));
-    const engine::Chunk chunk_snd_laugh_female1(snd_laugh_female1, sizeof(snd_laugh_female1));
-    const engine::Chunk chunk_snd_laugh_female2(snd_laugh_female2, sizeof(snd_laugh_female2));
-    const engine::Chunk chunk_snd_laugh_male1(snd_laugh_male1, sizeof(snd_laugh_male1));
-    const engine::Chunk chunk_snd_laugh_male2(snd_laugh_male2, sizeof(snd_laugh_male2));
-    const engine::Chunk chunk_snd_laugh_male3(snd_laugh_male3, sizeof(snd_laugh_male3));
-    const engine::Chunk chunk_snd_steps(snd_steps2, sizeof(snd_steps2));
+
+namespace {
+
+std::vector<engine::Chunk> init_snd_laugh() {
+    std::vector<engine::Chunk> result;
+    result.emplace_back(snd_laugh_child1, sizeof(snd_laugh_child1));
+    result.emplace_back(snd_laugh_female1, sizeof(snd_laugh_female1));
+    result.emplace_back(snd_laugh_female2, sizeof(snd_laugh_female2));
+    result.emplace_back(snd_laugh_male1, sizeof(snd_laugh_male1));
+    result.emplace_back(snd_laugh_male2, sizeof(snd_laugh_male2));
+    result.emplace_back(snd_laugh_male3, sizeof(snd_laugh_male3));
+    result.emplace_back(snd_steps2, sizeof(snd_steps2));
+    return result;
+}
+
+}
+
+const std::vector<engine::Chunk> snd_laugh = init_snd_laugh();
+
+/*const engine::Chunk chunk_snd_steps(snd_steps2, sizeof(snd_steps2));*/
+
 }
--- src/audio_data.h
+++ src/audio_data.h
@@ -1,13 +1,10 @@
 #pragma once
 
 #include <chunk.h>
+#include <vector>
 
 namespace game {
-	extern const engine::Chunk chunk_snd_laugh_child1;
-	extern const engine::Chunk chunk_snd_laugh_female1;
-	extern const engine::Chunk chunk_snd_laugh_female2;
-	extern const engine::Chunk chunk_snd_laugh_male1;
-	extern const engine::Chunk chunk_snd_laugh_male2;
-	extern const engine::Chunk chunk_snd_laugh_male3;
-	extern const engine::Chunk chunk_snd_steps;
+
+extern const std::vector<engine::Chunk> snd_laugh;
+
 }
