769b40463f46236dbc9ecad129b2ee037f8c8c16
afx1337 <afx1337@afx1337.app>
2024-01-28 12:00:36 +0100

    Add mumble and drag/drop audio

--- src/audio_data.h
+++ src/audio_data.h
@@ -10,11 +10,17 @@ public:
     AudioData();
 
     void play_laugh(int index) const;
+    void play_mumble(int index) const;
     void play_steps() const;
+    void play_drag() const;
+    void play_drop() const;
 
 private:
     std::vector<engine::Chunk> snd_laugh;
+    std::vector<engine::Chunk> snd_mumble;
     engine::Chunk snd_steps;
+    engine::Chunk snd_drag;
+    engine::Chunk snd_drop;
 };
 
 }
